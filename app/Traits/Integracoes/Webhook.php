74383d1982121588cef562ebb44b9fd617879465
Marne Rios <marne.rios@gmail.com>
2022-08-06 16:11:33 -0300

    Altera texto inicial

--- app/Traits/Integracoes/Webhook.php
+++ app/Traits/Integracoes/Webhook.php
@@ -81,7 +81,7 @@ trait Webhook {
                 'message'=>$text
             ];
         }
-        if (preg_match("/(quero um[^^]+(BMG|PAN))|(#)/i",$text)){
+        if (preg_match("/(quero saber[^^]+(BMG|PAN))|(#)/i",$text)){
             return [
                 'chatName'=>$dados['chatName'],
                 'phone'=>$dados['phone'],
